bf6bfce95e8493152394c8adef1f82d12e7bf48e
eleniVR <eleni.vraimaki@gmail.com>
2022-03-19 10:31:39 +0200

    hello android!2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,33 +14,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
-
-            val myFirstButton : Button =
-                findViewById(R.id.button)
-
-             fun rollDice(){
+//METAVLITI
+            val myFirstButton : Button = findViewById(R.id.button)
+//FUNCTION
+            fun rollDice(){
                 //δημιουργια ενος ζαριου
                 val firstDice = Dice(5)
                 //δημιουργία ρίψης
                 val diceRoll = firstDice.roll()
-
-                val resulTextViewMine : TextView =
-                    findViewById(R.id.textView)
+                //ΟΡΙΣΜΟΣ
+                val resulTextViewMine : TextView = findViewById(R.id.textView)
+                //ΟΡΙΣΜΟΣ ΚΕΙΜΕΝΟ
                 resulTextViewMine.text= diceRoll.toString()
                     }
 
-
+//PATIMA KOUMPIA
             myFirstButton.setOnClickListener {
                 val toastmsg = Toast.makeText(this, "hello there" , Toast.LENGTH_LONG)
                 toastmsg.show()
+                rollDice()
             }
         }
     }
 
-class Dice(val numSides: Int){
-
-    fun roll():Int {
-        return (1..numSides).random()
+class Dice(val numSides: Int){     
+            fun roll():Int {
+                    return (1..numSides).random()
     }
-
 }
